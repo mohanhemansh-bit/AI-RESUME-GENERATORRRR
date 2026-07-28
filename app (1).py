be29b1f007ff387a838e266fc076df92d4b02bb2
mohanhemansh-bit <mohanhemansh@gmail.com>
2026-07-28 12:47:17 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -130,8 +130,6 @@ if st.button('generate resume'):
     st.html(code , width="stretch" , unsafe_allow_javascript=True)
       
     response = agent.invoke({'messages': [{'role':'user','content':query}]})
-    print(response['messages'][-1].content)
     code=response['messages'][-1].content[-1]['text']
     #st.markdown(code)
-    st.html(code , width="stretch" , unsafe_allow_javascript=True)
     
